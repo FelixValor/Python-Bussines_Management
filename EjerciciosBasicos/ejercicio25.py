caec9ce29ef34a0b5452380be5e826c2ed43257c
FelixValor <valorfelix@gmail.com>
2022-01-10 09:05:24 +0100

    Todo lo realizado hasta el 10 de enero de 2022

--- EjerciciosBasicos/ejercicio25.py
+++ EjerciciosBasicos/ejercicio25.py
@@ -3,20 +3,29 @@ class Punto:
         self.x = x
         self.y = y
     
-    def __str__(self):
-        return self.x+","+self.y
+    def _str_(self):
+        return  "({}, {})".format(self.x, self.y)
     
     
     def cuadrante(self):
-        if(self.x==0&self.y!=0):
-            print("1º cuadrante")
-        if(self.x!=0&self.y==0):
-            print("2º cuadrante")
-        if(self.x==0&self.y!=0):
-            print("3º cuadrante")
-        if(self.x==0&self.y!=0):
-            print("4º cuadrante")
-            
+        if self.x==0 and self.y==0:
+            print ('Origen')
+        if self.x==0:
+            print ('Eje Y')
+        if self.y==0:
+            print ('Eje X')
+        if self.x>0 and self.y>0:
+            print ('Cuadrante 1')
+        if self.x<0 and self.y>0:
+            print ('Cuadrante 2')
+        if self.x<0 and self.y<0:
+            print ('Cuadrante 3')
+        if self.x>0 and self.y<0:
+            print ('Cuadrante 4')
+    
+    def vector(self,p):
+        print("El vector entre {} y {} es ({}, {})".format(
+            self, p, p.x - self.x, p.y - self.y))
 
 class Rectangulo:
     def __init__(self, x, y):
@@ -24,25 +33,37 @@ class Rectangulo:
         self.y = y
 
 menu=True
+print("Introduzca el primer punto:")
+p1=input()
+print("Introduzca el segundo punto:")
+p2=input()
+p = Punto(int(p1),int(p2))
 while menu:
     print("1 Operaciones con puntos:")
     print("2 Operaciones con rectangulos:")
     print("3 Salir")
     orden=input()
     match orden:
-        case 1:
+        case "1":
             print("a. Mostrar cuadrante al que pertenecen")
             print("b. Calcular vector")
             print("c. Calcular distancia")
             orden=input()
             match orden:
                 case "a":
-                    print("Cuadrante")
-                    Punto.cuadrante()
-        case 2:
+                    p.cuadrante()
+                case "b":
+                    print("Introduzca un nuevo punto")
+                    print("primer punto:")
+                    p1Vector=input()
+                    print("segundo punto:")
+                    p2Vector=input()
+                    pVector=Punto(int(p1Vector),int(p2Vector))
+                    p.vector(pVector)
+        case "2":
             print("a. Calcular base")
             print("b. Calcular altura")
             print("c. Calcular area")
-        case 3:
+        case "3":
             print("fin del programa")
             menu=False
