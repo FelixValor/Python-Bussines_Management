0c7ee6b6b8def234f6befdd4fdde4f35b6258533
Felix Valor <85871798+FelixValor@users.noreply.github.com>
2022-04-11 14:21:22 +0200

    Delete EjerciciosBasicos directory

--- EjerciciosBasicos/sge_t05_e26_valorfelix/modelo/Movimiento.py
+++
@@ -1,9 +0,0 @@
-#from Sge_env_EjerciciosPython.EjerciciosBasicos.sge_t05_e26_valorfelix.modelo.modelo_fecha import Fecha
-import Fecha
-
-class Movimiento:
-    def __init__(self,cantidad,esIngreso,concepto):
-        self.fecha=Fecha.fechaActual()
-        self.cantidad=cantidad
-        self.esIngreso=esIngreso
-        self.concepto=concepto
--- EjerciciosBasicos/sge_t05_e26_valorfelix/modelo/Prueba.py
+++
@@ -1,9 +0,0 @@
-from modelo.Cuenta import Cuenta
-
-
-p=Cuenta("87655678K", "felix", 79, "a")
-p2=Cuenta("87655678K", "felix", 79, "a")
-p3=Cuenta("87655678K", "felix", 79, "a")
-p.imprimir_cuenta()
-p2.imprimir_cuenta()
-p3.imprimir_cuenta()
